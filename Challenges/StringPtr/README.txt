f1837a640626403ce2409fd2de7bf886650036ba
cjzufelt <cjzufelt@gmail.com>
2019-06-01 14:14:40 -0600

    Completed the StringPtr challenge

---
+++ Challenges/StringPtr/README.txt
@@ -0,0 +1,2 @@
+In this challenge, 10 8-character strings are created. Then, a pointer points to one of them. The goal is to read through the assembly to figure out which string is being pointed at, and to figure out what string value is at that location.
+Answer: sentinel
---
+++ Challenges/StringPtr/src/StringPtr.c
@@ -0,0 +1,16 @@
+int main() {
+    char string0[9] = "illusion";
+    char string1[9] = "nautilus";
+    char string2[9] = "installs";
+    char string3[9] = "national";
+    char string4[9] = "nonsense";
+    char string5[9] = "solution";
+    char string6[9] = "sentinel";
+    char string7[9] = "telltale";
+    char string8[9] = "arsenals";
+    char string9[9] = "atlantis";
+
+
+    char* ptr = string6;
+    return 0;
+}

0bd43d30ff81129d311cb439bcbec8c1a2954078
cjzufelt <cjzufelt@gmail.com>
2019-06-11 18:00:34 -0600

    Fleshing out the secret menu. Trying to use bitwise operators on string

--- Challenges/6.FirstInput/src/FirstInput.cpp
+++ Challenges/6.FirstInput/src/FirstInput.cpp
@@ -1,36 +1,62 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::setw;
 using std::ostringstream;
 
-void login() {
-    cout << "Welcome" << endl;
+void gnu_x86_64() {
+    cout << "Root access granted";
 }
 
-/*void openingInput(const string& input) {
-    switch(input) {
-        case "LOGIN":
-            login();
-            break;
+void shutdown() {
 
-        case"OBFUSCATE":
-            obfuscate();
-            break;
+}
 
-        case "QUIT":
-            break;
+void login() {
+    cout << "Successfully logged in.";
+}
 
-        default:
-            openingInput(input);
+string commandsString() {
+    ostringstream oss;
+    oss << "Commands:" << endl;
+    oss << "Login" << setw(20) << "Shutdown" << endl;
+    return oss.str();
+}
+
+void normalizeInput(string& input) {
+    for (int i = 0; i < input.length(); ++i) {
+        input.at(i) = toupper(input.at(i));
     }
-}*/
-string everytimeOutput() {
+}
+
+int system_x86_64(string input) {
+    try {
+        char substr[6];
+        for (int i = 0; i < 6; ++i) {
+            substr[i] = input.at(i+1);
+        }
+
+        cout << "Completed try-catch" << endl;
+    }
+    catch(...) {}
+    return 0;
+}
+
+string receiveInput() {
+    string input;
+    cin >> input;
+    if (system_x86_64(input) == 0xfacade) {
+        gnu_x86_64();
+    }
     
+    normalizeInput(input);
+    return input;
 }
 
 string greeting() {
@@ -41,27 +67,24 @@ string greeting() {
     return oss.str();
 }
 
-string receiveInput() {
-    string input;
-    cin >> input;
-    normalizeInput(input);
-    return input;
-}
-
-void normalizeInput(string& input) {
-    for (int i = 0; i < input.length(); ++i) {
-        input.at(i) = toupper(input.at(i));
-    }
-}
-
 int main() {
     cout << greeting();
     string input;
-    cout << "Enter command: ";
-    cin >> input;
 
-    normalizeInput(input);
-    //openingInput()
+    bool inputInvalid = true;
+    while (inputInvalid) {
+        cout << commandsString();
+        input = receiveInput();
+        cout << endl;
+
+        if (input == "LOGIN") {
+            inputInvalid = false;
+            login();
+        }
+        else if (input == "SHUTDOWN") {
+            inputInvalid = false;
+        }
+    }
 
     return 0;
 }

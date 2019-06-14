da2018146987e3cb6575b7a2eca79d2f1ed08b38
cjzufelt <cjzufelt@gmail.com>
2019-06-14 17:30:04 -0600

    Finished challenge 6. It's a doozy

--- Challenges/6.FirstInput/src/FirstInput.cpp
+++ Challenges/6.FirstInput/src/FirstInput.cpp
@@ -10,80 +10,108 @@ using std::string;
 using std::setw;
 using std::ostringstream;
 
-void func() {
+enum Commands {LOGIN, SHUTDOWN, LOGOFF, SECRET};
+
+void normalizeInput();
+string receiveInput();
+void escalate();
+void shutdown();
+string loginCommands();
+void loginMenu();
+void login();
+void rootMenu();
+string rootCommands();
+string greeting();
+
+void escalate() {
     cout << "Root access granted";
 }
 
-void shutdown() {
-    cout << "Goodbye!" << endl;
+string loginCommands() {
+    ostringstream oss;
+    oss << "Commands:" << endl;
+    oss << "Echo" << setw(20) << "Logoff" << endl;
+    return oss.str();
+}
+
+void loginMenu() {
+    string input;
+
+    while (true) {
+        cout << loginCommands(); 
+        input = receiveInput();
+        cout << endl;
+
+        if (input.substr(0,5) == "ECHO ") {
+            cout << input.substr(5) << endl << endl;
+        }
+        else if (input == "LOGOFF") {
+            cout << "Successfully logged off" << endl << endl;
+            return;
+        }
+        else if (input.at(0) == 0x6) {
+            escalate();
+        }
+    }
 }
 
 void login() {
-    cout << "Successfully logged in." << endl;
+    cout << "Successfully logged in" << endl << endl;
+    loginMenu();
 }
 
-string openingCommands() {
+string rootCommands() {
     ostringstream oss;
     oss << "Commands:" << endl;
     oss << "Login" << setw(20) << "Shutdown" << endl;
     return oss.str();
 }
 
+void rootMenu() {
+    string input;
+
+    while (true) {
+        cout << rootCommands(); 
+        input = receiveInput();
+        cout << endl;
+
+        if (input == "LOGIN") {
+            login();
+        }
+        else if (input == "SHUTDOWN") {
+            cout << "Goodbye!" << endl;
+            return;
+        }
+    }
+}
+
+string greeting() {
+    ostringstream oss;
+    oss << setw(28) << "Welcome!" << endl;
+    oss << "Enter one of the following commands to continue: " << endl << endl;
+
+    return oss.str();
+}
+
 void normalizeInput(string& input) {
     for (int i = 0; i < input.length(); ++i) {
+        if (isspace(input.at(i))) {
+            break;
+        }
         input.at(i) = toupper(input.at(i));
     }
 }
 
 string receiveInput() {
     string input;
-    cin >> input;
+    getline(cin, input);
     normalizeInput(input);
     return input;
 }
 
-string greeting() {
-    ostringstream oss;
-    oss << setw(28) << "Welcome!" << endl;
-    oss << "Enter one of the following commands to continue: " << endl;
-
-    return oss.str();
-}
-
-int openingMenu() {
-    cout << greeting();
-    string input;
-
-    while (true) {
-        cout << openingCommands(); 
-        input = receiveInput();
-        cout << endl;
-
-        if (input == "LOGIN") {
-            return 1;
-        }
-        else if (input == "SHUTDOWN") {
-            return 2;
-        }
-        else if (input.at(0) == 0x6) {
-            return 3;
-        }
-    }
-}
-
 int main() {
-    switch (openingMenu()) {
-        case 1:
-            login();
-            break;
+    cout << greeting();
+    rootMenu();
 
-        case 2:
-            shutdown();
-            break;
-
-        case 3:
-            func();
-            break;
-    }
     return 0;
 }
